eb88e79c21196ec1935e67d491acaac49d6b2b1c
Angel sopena <angelsopena@Angels-MacBook-Pro.local>
2019-03-23 16:24:24 -0700

    Create Add item

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,66 @@
+// Getting Elements
+
+const itemForm = document.getElementById('itemForm');
+const itemInput = document.getElementById('itemInput');
+const itemList = document.getElementById('item-list');
+const ClearBtn = document.getElementById('clear-list');
+const feedback = document.querySelector('.feedback');
+const addBtn = document.getElementById('add');
+
+
+let itemData = [];
 
 
 
-    //Add an item
+// Form Submission
 
-    //Check if is any text in the input before adding 
+// This will prevent the form from loadning the page when a user press the submit btn as default 
+itemForm.addEventListener('submit', function(event){
+    event.preventDefault();
 
-    //Adding it to a list 
+const textValue = itemInput.value;
 
-    //Giving a remove option 
+ 
+if (textValue === ''){
+    addBtn.addEventListener('click' , M.toast({html: 'Enter a Valid Input'}));
+} else {
+    addItem(textValue);
+    itemInput.value = '';
+    itemData.push(textValue);
+
+    deleteNote(textValue); 
+    
+}
+});
+
+// Add item to list
+function addItem(value){
+const div = document.createElement('div');
+div.classList.add('item', 'cool')
+div.innerHTML = `<a href="#!" class="collection-item"> ${value} <i class="material-icons right">remove_circle_outline</i></a>`
+
+itemList.appendChild(div);
+
+
+};
+
+
+// Delete Item from List 
+function deleteNote(textValue) {
+const items = itemList.querySelectorAll('.item');
+ items.forEach(function(item){
+     
+    item.querySelector('.material-icons').addEventListener('click', function() {
+        itemList.removeChild(item);
+
+        itemData = itemData.filter(function(item){
+            
+        return item !== textValue;
+        });
+
+        M.toast({html: 'You have delted an item'});
+
+    });
+
+ });
+};
