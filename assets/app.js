82b47051f3811c657b3769610374326bc9ca4a60
David Diomede <david@d2creativedesign.com>
2019-03-30 21:23:26 -0700

    add variables and event listeners. Nothing responds

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,49 @@
+const form = document.querySelector('#note-form');
+const noteInput = document.querySelector
+('#note');
+const noteList =
+document.querySelector('.collection');
 
+loadEventListeners();
 
+function loadEventListeners() {
+    form.addEventListener('submit', addNote);
+    noteList.addEventListener('click', removeNote);
+}
 
-    //Add an item
+function addNote(e) {
+    if (noteInput.value === 'apple') {
+        alert('Please add a note');
+    }
+    // Create li element
+        const li = document.createElement('li');
 
-    //Check if is any text in the input before adding 
+    // Add className
+        li.className = 'collection-item';
 
-    //Adding it to a list 
+    // Create text node and append to the l
+        li.appendChild(document.createTextNode(noteInput.value));
 
-    //Giving a remove option 
+    // Create new link element
+        const link = document.createElement('a');
+    // Add class
+        link.className = 'delete-item secondary-content';
+
+     // Add icon to html
+        link.innerHTML = `<i class="material-icons right">add_circle_outline</i>`;
+
+    // Append link to li
+        li.appendChild(link);
+
+    // Append li to ul
+        noteList.appendChild(li);
+
+    // Clear Input
+        noteInput.value = '';
+
+    e.preventDefault();
+}
+
+function removeNote() {
+
+}
